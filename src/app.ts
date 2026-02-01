376d3e227643589072a97d355930df20432690aa
jashanpreet <jsdihu55@academic.rrc.ca>
2026-02-01 11:49:48 -0600

    feat: add v1 health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import healthRouter from "./api/v1/routes/health";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/v1", healthRouter);
 
 export default app;
+
